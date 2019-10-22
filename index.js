10ca6b4977a13c4c40c93bcaa3a2083a061b9420
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-10-22 18:50:21 -0500

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 var message = document.createElement("p");
-var nodo = document.createTextNode("What important is learn Grammar");
+var nodo = document.createTextNode("What important is learn Grammar as one of the most important thing");
 
 message.appendChild(nodo);
 document.body.appendChild(nodo);
