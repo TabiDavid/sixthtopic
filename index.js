0179f85616599eb4c2361e5e572e36f7bbcad2b5
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-10-21 21:19:40 -0500

    commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
-
 var message = document.createElement("h2");
 var content = document.createTextNode("What important is learn Grammar");
-element.appendChild(content);
-element.setAttribute("align", "center");
-document.getElementById("second").appendChild(message);
+message.appendChild(content);
+
+var element = document.getElementById("second");
+element.appendChild(message);
