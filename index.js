cf57d89db50b7325add1dc66ca3d7f1690d0f870
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-10-22 19:05:04 -0500

    commit

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
 
-var message = document.createElement("p");
-var nodo = document.createTextNode("What important is learn Grammar as one of the most important thing");
+var message = document.createElement("h2");
+var nodo = document.createTextNode("What important is learn Grammar as one of the most important thing ok");
 
 message.appendChild(nodo);
-document.body.appendChild(nodo);
+document.body.appendChild(message);
+
+var second = document.createElement("h3");
+var figure = document.createTextNode("Something that I do not Understand");
+
+second.appendChild(figure);
+document.body.appendChild(second)
