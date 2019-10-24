a8bb8da0fbfede0862c57a00402db516d3c17ca2
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-10-23 20:12:27 -0500

    commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 
-var message = document.createElement("h2");
+/*var message = document.createElement("h2");
 var nodo = document.createTextNode("What important is learn Grammar as one of the most important thing ok");
 
 message.appendChild(nodo);
-document.body.appendChild(message);
+document.body.appendChild(message);*/
 
-var second = document.createElement("h3");
+/*var second = document.createElement("h3");
 var figure = document.createTextNode("Something that I do not Understand");
 
 second.appendChild(figure);
-document.body.appendChild(second);
+document.body.appendChild(second);*/
 
 
