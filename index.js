7ae2f51a27214df0b1c873824756068692ff209b
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-10-26 11:41:20 -0500

    commit

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-
-
-/*var message = document.createElement("h2");
-var nodo = document.createTextNode("What important is learn Grammar as one of the most important thing ok");
-
-message.appendChild(nodo);
-document.body.appendChild(message);*/
-
-/*var second = document.createElement("h3");
-var figure = document.createTextNode("Something that I do not Understand");
-
-second.appendChild(figure);
-document.body.appendChild(second);*/
-//<script>
-
 function heading2(){
     var message = document.createElement("h2");
 var nodo = document.createTextNode("What important is learn Grammar as one of the most important thing");
@@ -63,7 +48,6 @@ function addTothelist(){
 function erase(){
 
     var desappear = document.getElementById('stepone');
-    
+
     desappear.remove();
 }
-//</scritp>
