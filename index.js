83a25b08e519a6d053c858d49397facd87707dfa
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2019-10-26 10:05:37 -0500

    commit

--- index.js
+++ index.js
@@ -31,31 +31,34 @@ second.appendChild(figure);
 document.getElementById("titlethree").appendChild(second);
 
 
+};
+
+
+function parragraph(){
+
+    const myList = document.getElementById('myList');
+    const pearsItem = document.getElementById('pearsItem');
+    
+    console.log(myList);
+    console.log(pearsItem);
+    
+    const orangesItem = document.createElement('p');
+    orangesItem.textContent = '2._What is happening here with our language? we never know how important is learn a new language but is sure that all of us have chance to do it';
+    
+    myList.insertBefore(orangesItem, pearsItem);
+};
+
+
+function addTothelist(){
+    var stepone = document.createElement('li');
+    var vocabulary = document.createTextNode('Vocabulary');
+    stepone.appendChild(vocabulary);
+    
+    var element = document.getElementById('languageAcqui');
+    var child = document.getElementById('stepfour');
+    element.insertBefore(stepone, child);
+    
 }
-
-
-const myList = document.getElementById('myList');
-const pearsItem = document.getElementById('pearsItem');
-
-console.log(myList);
-console.log(pearsItem);
-
-const orangesItem = document.createElement('p');
-orangesItem.textContent = '2._What is happening here with our language?';
-
-myList.insertBefore(orangesItem, pearsItem);
-
-
-
-
-var stepone = document.createElement('li');
-var vocabulary = document.createTextNode('Vocabulary');
-stepone.appendChild(vocabulary);
-
-var element = document.getElementById('languageAcqui');
-var child = document.getElementById('stepfour');
-element.insertBefore(stepone, child);
-
 
 function erase(){
 
